b9682752dcd241ee7ec4dd0a65039d3b90dafe37
StonyBaboon <28043982+StonyBaboon@users.noreply.github.com>
2023-08-04 18:18:20 +0100

    Update defender_toggle.bat

--- defender_toggle.bat
+++ defender_toggle.bat
@@ -1,5 +1,18 @@
 @echo off
 
+REM Detect if the script is already running with elevated privileges
+NET SESSION >NUL 2>NUL
+if %ERRORLEVEL% EQU 0 (
+    goto :main
+) else (
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%TEMP%\elevate.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%TEMP%\elevate.vbs"
+    "%TEMP%\elevate.vbs"
+    del "%TEMP%\elevate.vbs"
+    exit /b
+)
+
+:main
 REM Set default language to English (United States)
 chcp 437 > nul
 
@@ -13,24 +26,30 @@ if '%errorlevel%' NEQ '0' (
 
 setlocal enabledelayedexpansion
 
-:input
+:loop
 echo.
 echo Choose the action you want to perform:
-echo   on  - To turn on Windows Defender
-echo   off - To turn off Windows Defender
+echo   on    - To turn on Windows Defender
+echo   off   - To turn off Windows Defender
+echo   exit  - To exit this script
 set /p choice="Enter your choice: "
 
 if /i "!choice!"=="on" (
     echo Turning on Windows Defender...
-    powershell.exe -command "Set-MpPreference -DisableRealtimeMonitoring \$false"
+    powershell.exe -command "Set-MpPreference -DisableRealtimeMonitoring:$false"
     echo Windows Defender has been turned on.
+    goto loop
 ) else if /i "!choice!"=="off" (
     echo Turning off Windows Defender...
-    powershell.exe -command "Set-MpPreference -DisableRealtimeMonitoring \$true"
+    powershell.exe -command "Set-MpPreference -DisableRealtimeMonitoring:$true"
     echo Windows Defender has been turned off temporarily.
+    goto loop
+) else if /i "!choice!"=="exit" (
+    echo Exiting the script...
+    pause
+    exit /b
 ) else (
-    echo Invalid option. Please enter "on" or "off".
-    goto input
+    echo Invalid option. Please enter "on", "off", or "exit".
+    goto loop
 )
 
-pause
